d3dc6fec9a0ddb33a01f436adcb17685230d393d
Anujit Nene <anujitnene@gmail.com>
2025-01-26 12:21:26 +0530

    use modal instead of dialog

--- src/components/PokemonDetails/PokemonDetails.tsx
+++ src/components/PokemonDetails/PokemonDetails.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent, DialogTitle } from '@mui/material';
+import { Modal } from '@mui/material';
 import { createUseStyles } from 'react-jss';
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useGetPokemon } from '../../hooks/useGetPokemon';
@@ -19,53 +19,76 @@ export const PokemonDetails = () => {
   console.log('pokemonFeatures', pokemonFeatures);
 
   return (
-    <Dialog
+    <Modal
+      className={classes.modal}
       onClose={() => {
         navigate(-1);
       }}
       open={true}
     >
-      <DialogTitle className={classes.dialogContent}>
-        {pokemon.number} {pokemon.name}
-      </DialogTitle>
-      <DialogContent className={classes.dialogContent}>
-        {!pokemonFeatures ? (
-          <div>Loading...</div>
-        ) : (
-          <>
-            <p>{pokemonFeatures.classification}</p>
-            <div>
-              <h2>RESISTANT</h2>
-              <p>{pokemonFeatures.resistant.join(', ')}</p>
-            </div>
-            <div>
-              <h2>WEAKNESSES</h2>
-              <p>{pokemonFeatures.weaknesses.join(', ')}</p>
-            </div>
-            <div>
-              <h2>HEIGHT</h2>
-              <p>
-                <span>Minimum </span>
-                <span>{pokemonFeatures.height.minimum}</span>
-                <span> Maximum </span>
-                <span>{pokemonFeatures.height.maximum}</span>
-              </p>
-            </div>
-            <div>
-              <h2>TYPES</h2>
-              <p>{pokemonFeatures.types.join(', ')}</p>
-            </div>
-          </>
-        )}
-      </DialogContent>
-    </Dialog>
+      <div className={classes.modalContent}>
+        <div className={classes.header}>
+          <h2>
+            {pokemon.number} {pokemon.name}
+          </h2>
+          <Link to="/pokemon">Back</Link>
+        </div>
+        <div>
+          {!pokemonFeatures ? (
+            <div>Loading...</div>
+          ) : (
+            <>
+              <p>{pokemonFeatures.classification}</p>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div>
+                  <h2>RESISTANT</h2>
+                  <p>{pokemonFeatures.resistant.join(', ')}</p>
+                </div>
+                <div>
+                  <h2>WEAKNESSES</h2>
+                  <p>{pokemonFeatures.weaknesses.join(', ')}</p>
+                </div>
+              </div>
+              <div>
+                <h2>HEIGHT</h2>
+                <p>
+                  <span>Minimum </span>
+                  <span>{pokemonFeatures.height.minimum}</span>
+                  <span> Maximum </span>
+                  <span>{pokemonFeatures.height.maximum}</span>
+                </p>
+              </div>
+              <div>
+                <h2>TYPES</h2>
+                <p>{pokemonFeatures.types.join(', ')}</p>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </Modal>
   );
 };
 
 const useStyles = createUseStyles(
   {
-    dialogContent: {
+    header: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    modal: {
+      width: '100%',
+      alignItems: 'center',
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    modalContent: {
       background: '#171E2b',
+      padding: '20px',
+      width: '70%',
+      borderRadius: '12px',
+      minHeight: '400px',
     },
   },
   {
